6517db1c72dd5ddbed1b7452cfefef474b05fd5e
Luiz <34900176+lpessin@users.noreply.github.com>
2019-07-13 17:09:18 -0300

    Create finder.py

---
+++ finder.py
@@ -0,0 +1,21 @@
+from steem import Steem
+from steem.account import Account
+import csv
+
+atlas = ['BRASIL', 'BRAZIL']
+ac = Account('lpessin')
+s = Steem()
+all = []
+with open('all', mode='r') as inall:
+    leitor = csv.reader(inall)
+    for linha in leitor:
+        l = linha[0]
+        all.append(l)
+
+
+for br in all:
+    ac = Account(br)
+    local = str(ac.profile).upper()
+    for a in atlas:
+        if a in local:
+            print(ac.name)
