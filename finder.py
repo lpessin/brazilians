888e186f7487cd22a4e642a554f17a7aa44778bb
Luiz <34900176+lpessin@users.noreply.github.com>
2019-07-13 19:26:55 -0300

    Update finder.py

--- finder.py
+++ finder.py
@@ -1,7 +1,7 @@
 from steem import Steem
 from steem.account import Account
 import csv
-
+import openpyxl as op
 atlas = ['BRASIL', 'BRAZIL']
 ac = Account('lpessin')
 s = Steem()
@@ -11,12 +11,22 @@ with open('all', mode='r') as inall:
     for linha in leitor:
         l = linha[0]
         all.append(l)
+p = ac.profile.get('location').upper()
+print(p)
 
 
+wb = op.load_workbook('br.xlsx')
+ws = wb['Sheet1']
+ws2 = wb['Sheet2']
+c = 0
 for br in all:
     ac = Account(br)
-    #print(ac.name, ac.profile.get('location'))
-    local = str(ac.profile.get('location')).upper()
-    for a in atlas:
-        if a in local:
-            print(ac.name, ac.profile.get('location'))
+    c += 1
+    if c <= 500000:
+        ws.append([ac.name, ac.profile.get('location')])
+    else:
+        print('metade!!')
+        ws2.append([ac.name, ac.profile.get('location')])
+ 
+wb.save('br.xlsx')
+wb.close()
